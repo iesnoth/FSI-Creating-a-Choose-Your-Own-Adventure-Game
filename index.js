8911ae9971f0891fa9b1de29c72c32e4d839c332
Victoria Beasley <iesnoth91@gmail.com>
2022-04-27 11:29:02 -0500

    second answer

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 //Your Code here
-let firstAnswer = window.prompt('Do you head left or right?')
-console.log(firstAnswer);
-
-if(firstAnswer === 'right'){
-    console.log('you turn right')
-   let secondAnswer = window.prompt('You went right, now up or down?')
-
-   if(secondAnswer === 'up')
+let firstAnswer= window.prompt('You are at a crossroads. Do you go left or right?')
+console.log(firstAnswer)
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt('You come across a stray cat. You try to pet it (you are very firendly), but it runs off before you can. Do you follow it, or continue on your path?')
 }
-else if(firstAnswer === 'left'){
-    console.log('you turn to the left')
+else if(firstAnswer === 'right'){
+    let secondAnswer = window.prompt('You come across a snoring dragon. On the other side of him you see a chest of treasure. Another path would lead you away from the dragon altogether. Which path do you take?')
 }
-else{
-    console.log('choose left or right')
-}
+if (secondAnswer )
