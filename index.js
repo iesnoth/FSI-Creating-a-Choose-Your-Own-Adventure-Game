693ea421fd50935957c5d984372442d7961fbdbb
Victoria Beasley <iesnoth91@gmail.com>
2022-04-26 20:45:58 -0500

    window prompts

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your Code here
+let firstAnswer = window.prompt('Do you head left or right?')
+console.log(firstAnswer);
+
+if(firstAnswer === 'right'){
+    console.log('you turn right')
+   let secondAnswer = window.prompt('You went right, now up or down?')
+
+   if(secondAnswer === 'up')
+}
+else if(firstAnswer === 'left'){
+    console.log('you turn to the left')
+}
+else{
+    console.log('choose left or right')
+}
